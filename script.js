d7c928f3267172f86d4f13c602ab0a5138c1d0d7
wandersontx <wandersonh89@gmail.com>
2020-06-13 18:01:11 -0300

    Sequencia de excução de código

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+	function teste() {
+	console.log($('#exemplo'))
+}
+teste()
+})
+
+/*
+modo alternatvio
+function teste() {
+	console.log($('#exemplo'))
+}
+
+//Passando apenas a referencia da função.
+$(teste)	
+*/
