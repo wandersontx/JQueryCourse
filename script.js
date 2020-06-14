6c558b7ac061030416338ec081bb28cdcf48f2e2
wandersontx <wandersonh89@gmail.com>
2020-06-13 21:51:37 -0300

    Recuperando e modificando atributos

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
+/*
 $(document).ready(function(){
 	function teste() {
 	console.log($('#exemplo'))
 }
 teste()
-})
+})*/
 
-/*
+
 modo alternatvio
 function teste() {
 	console.log($('#exemplo'))
 }
 
 //Passando apenas a referencia da função.
-$(teste)	
-*/
+	
